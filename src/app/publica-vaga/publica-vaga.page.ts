5db0ded548524e4d33934ab67aad720ef5b18f90
Atropina <40372156+Atropina@users.noreply.github.com>
2020-11-17 17:15:58 -0300

    Inicio do cadastro de vagas

--- src/app/publica-vaga/publica-vaga.page.ts
+++ src/app/publica-vaga/publica-vaga.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { Vagas} from '../shared/interfaces/Vagas';
+import { VagaService } from '../shared/services/vaga/vaga.service'
 
 @Component({
   selector: 'app-publica-vaga',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publica-vaga.page.scss'],
 })
 export class PublicaVagaPage implements OnInit {
+  private cidade:string;
+  private rua:string;
+  private uf:string
 
-  constructor() { }
 
+  constructor(
+    private vagaService : VagaService
+  ) { }
+  empresaData = JSON.parse(localStorage.getItem("user"));
+  private empresa:string
+  private empresaUid:string
+
+  
   ngOnInit() {
+    this.empresa = this.empresaData.displayName
+    this.empresaUid = this.empresaData.uid
+  }
+
+  async autoCep(cep){
+    console.log(cep.value)
+    const res = await fetch("https://viacep.com.br/ws/"+cep.value+ "/json/");
+    const endereco = await res.json();
+    this.cidade = endereco.localidade;
+    this.rua = endereco.logradouro;
+    this.uf = endereco.uf
+  }
+  submitForm(formData){
+    const data : Vagas = {
+      nomeVaga: formData.value.nomevaga,
+      area: formData.value.atuacao,
+      descricao: formData.value.descricao,
+      requisitos: formData.value.requisistos,
+      cep: formData.value.cep,
+      salario: formData.value.salario,
+      endereco: this.rua,
+      complemento: formData.value.complemento,
+      empresa: this.empresa,
+      uid: this.empresaUid,
+    }
+
+    this.vagaService.addVaga(data)
   }
 
 }
