07b0892c299811aab7935b42272ce8b60f677899
Atropina <40372156+Atropina@users.noreply.github.com>
2020-11-19 15:17:18 -0300

    Correções CSS
    Corrigido o sidemenu
    ❗ Começado o perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [  
   {
     path: '',
-    redirectTo: 'esqueciasenha',
+    redirectTo: 'perfil',
     pathMatch: 'full'
   },
   {
